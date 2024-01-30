d52fbf2d5ea4479164aff84e95c05965c1db8719
Demian Tavolaro <demiantavolaro6@gmail.com>
2024-01-30 15:19:57 -0300

    Step 3.12

---
+++ mongo.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const password = process.argv[2],
+      name= process.argv[3],
+      number= process.argv[4]
+
+const URL = `mongodb+srv://demiantavolaro6:${password}@cluster0.jx8xzrh.mongodb.net/phonebookDB?retryWrites=true&w=majority`
+
+mongoose.set(`strictQuery`,false);
+mongoose.connect(URL);
+
+if(password.length < 3){
+
+    console.log('give password as argument')
+    process.exit(1)
+
+
+}
+
+const contactSchema= new mongoose.Schema({
+    name: String,
+    number: Number
+
+})
+
+const Contact = new mongoose.model(`Contact`, contactSchema);
+
+const contact = new Contact({
+    name,
+    number,
+
+})
+
+// contact.save()
+//  .then(result => {
+//     console.log(result);
+//     mongoose.connection.close();
+//  })
+
+ Contact.find({}).then(result => {
+    result.forEach(contact => {
+        console.log(contact);
+    })
+
+    mongoose.connection.close();
+ })
