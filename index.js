031cc2bf39de0c2b62e668a4e6ad6ac13282a006
Demian Tavolaro <demiantavolaro6@gmail.com>
2024-02-23 15:11:14 -0300

    Exercise 3.22.

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-import  express  from "express";
-import morgan from "morgan";
+import express from 'express';
+
+import morgan from 'morgan';
 import cors from 'cors';
-import Contact from "./models/contact.js";
+import Contact from './models/contact.js';
 
-
-let customLogFormat;
 const app = express();
 
-
-
-
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(express.static('dist'));
 
 app.use((req, res, next) => {
@@ -19,110 +15,47 @@ app.use((req, res, next) => {
   next();
 });
 
-morgan.token('body', (request) => JSON.stringify(request.body))
-customLogFormat = `:method :url :status - :response-time ms - Content-Type: :req[Content-Type] - Request Body: :body`;
-app.use(morgan(customLogFormat))
+morgan.token('body', (request) => JSON.stringify(request.body));
+const customLogFormat =
+  ':method :url :status - :response-time ms - Content-Type: :req[Content-Type] - Request Body: :body';
+app.use(morgan(customLogFormat));
 
-
-
-let persons= [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-      },
-      {
-        "name": "Demian Tavolaro",
-        "number": "6745456",
-        "id": 3
-      },
-      {
-        "name": "elvira",
-        "number": "12312312",
-        "id": 8
-      },
-      {
-        "name": "rafael",
-        "number": "4545",
-        "id": 9
-      },
-      {
-        "name": "josefina",
-        "number": "444",
-        "id": 11
-      },
-      {
-        "name": "carlos",
-        "number": "5",
-        "id": 12
-      },
-      {
-        "name": "fabi",
-        "number": "5555",
-        "id": 13
-      },
-      {
-        "name": "jorge",
-        "number": "323232",
-        "id": 14
-      },
-      {
-        "name": "julio",
-        "number": "21",
-        "id": 16
-      },
-      {
-        "name": "juancho",
-        "number": "23490984",
-        "id": 17
-      }
-]
-
-app.get("/api/persons", (request, response, next) => {
-
+app.get('/api/persons', (request, response, next) => {
   Contact.find({})
-   .then(contact =>{
-     response.json(contact)
+    .then((contact) => {
+      response.json(contact);
     })
-    .catch(error => next(error))
+    .catch((error) => next(error));
+});
 
+app.get('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params;
 
-
-
-})
-
-app.get("/api/persons/:id", (request,response,next) => {
-
-    const id = request.params.id;
-
-    Contact.findById(id)
-     .then(contact=>{
-      if(contact){
-        response.json(contact)
+  Contact.findById(id)
+    .then((contact) => {
+      if (contact) {
+        response.json(contact);
       } else {
-        response.status(404).end()
+        response.status(404).end();
       }
-       
-     })
-     .catch(error => next(error))
-})
+    })
+    .catch((error) => next(error));
+});
 
-app.delete("/api/persons/:id", (request,response, next) => {
+app.delete('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params;
 
-    const id = request.params.id;
-    
-    Contact.findByIdAndDelete(id)
-     .then(contact =>{
-       response.status(204).end()
-     })
-     .catch(error => next(error))
+  Contact.findByIdAndDelete(id)
+    .then((contact) => {
+      response.send(contact).status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
-    
-})
-
-app.post("/api/persons", (request,response,next) => {
-
-    const body = request.body;
+app.post(
+  '/api/persons',
+  (request, response, next) => {
+    const { body } = request;
 
     // const checkName = () => persons.some(person => person.name === body.name);
 
@@ -135,103 +68,94 @@ app.post("/api/persons", (request,response,next) => {
     //     })
     // } else {
 
-      const contact = new Contact({
-        name: body.name,
-        number: body.number,
+    const contact = new Contact({
+      name: body.name,
+      number: body.number,
+    });
+
+    contact
+      .save()
+      .then((res) => {
+        console.log('Contacto guardado');
+        response.json(res);
       })
+      .catch((error) => next(error));
+  }
 
-      contact.save()
-      .then(contact =>{
-        console.log("Contacto guardado")
-        response.json(contact)
-      })
-      .catch(error => next(error))
-    }
+  // if(checkName()){
 
+  //     response.status(409);
 
-    
-    // if(checkName()){
+  //     return response.json({
+  //         error: "Name must be unique"
+  //     })
+  // } else {
 
-    //     response.status(409);
-        
-    //     return response.json({
-    //         error: "Name must be unique"
-    //     })
-    // } else {
+  //     const person = {
+  //         name: body.name,
+  //         number:body.number,
+  //         id: generateID()
 
-    //     const person = {
-    //         name: body.name,
-    //         number:body.number,
-    //         id: generateID()
+  //     }
 
-    //     }
+  //     persons = persons.concat(person)
 
-    //     persons = persons.concat(person)
+  //     response.json(person);
+);
 
-    //     response.json(person);
-
-)
-
-app.put("/api/persons/:id", (req, res, next) =>{
-
-  const id = req.params.id;
-  const {name, number} = req.body
-
-  
+app.put('/api/persons/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { name, number } = req.body;
 
   Contact.findByIdAndUpdate(
-     id,
-     {name,number},
-     {new: true, runValidators: true, context: "query"})
-   .then(contactUpdated =>{
-    res.json(contactUpdated)
+    id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then((contactUpdated) => {
+      res.json(contactUpdated);
+    })
+    .catch((error) => next(error));
+});
 
-   })
-   .catch(error => next(error))
-})
+app.get('/info', (request, response) => {
+  const date = new Date();
 
-app.get("/info", (request, response) => {
-  
-const date = new Date();
-
-Contact.countDocuments({})
- .then(result => {
-   response.send(`
+  Contact.countDocuments({}).then((result) => {
+    response.send(`
     <p>Phonebook has info for ${result} people<p>
     <p>${date}<p>
-    `)
-})
-
-})
+    `);
+  });
+});
 
 // Error Middleware
 // Cuando se realice una peticion a una ruta inexistente, llega a este error, ruta no definida en servidor
-const routeError = (req, res, next) => {
-  console.log("Middleware Error Handling");
-  res.status(404).send({ error: 'unknown endpoint' })
-}
 
-app.use(routeError)
+const routeError = (req, res) => {
+  console.log('Middleware Error Handling');
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(routeError);
 
 const idError = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === "ValidationError"){
-    return response.status(400).send({error: error.message})
-
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 
+app.use(idError);
 
-app.use(idError)
+const { PORT } = process.env;
 
-const PORT = process.env.PORT;
-
-app.listen(PORT, () =>{
-
-    console.log("Servidor activo localmente...")
-})
+app.listen(PORT, () => {
+  console.log('Servidor activo localmente...');
+});
